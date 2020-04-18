8e769929eb1ca257afcf4f8fe6b827001ac8c8a0
kuntalsarkar20 <62298200+kuntalsarkar20@users.noreply.github.com>
2020-04-19 01:07:03 +0530

    Just Design

---
+++ Home.php
@@ -0,0 +1,74 @@
+<?php
+include "Header.php";
+?>
+<div class="container-fluid">
+	<img src="background.jpg" width="100%" height="600px">
+	<div class="centered"><center><h2 style="font-weight: bold;">Student Portal for Better UnderStanding</h2><br><h4>Contribute here by Writing Answers for your favourite Subjects and Help others to understand the Subject Easily.</h4><br><button type="button" class="btn btn-success btn-lg">Contribute Here</button></center></div>
+</div>
+<div class="container" style="padding-top: 20px;">
+	<h3 style="font-weight: bold;">Browse By Categories</h3>
+	<hr style="border:2px solid #66e066;">
+	<div class="row" style="padding-left: 30px;padding-right: 30px;">
+		<div class="col-md-5 col-lg-5 col-sm-12" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);padding: 0px 0px;">
+			<div class="col-sm-12" style="background-color: #66e066;padding:1px;"><h3>DBMS</h3></div>
+			<div style="padding: 10px 10px;">
+				<p style="border-bottom: 2px solid grey;padding:5px;">What is DBMS? <span style="float: right;">17th April,2020</span></p>
+				<p style="border-bottom: 2px solid grey;padding:5px;">What is DBMS? <span style="float: right;">17th April,2020</span></p>
+				<p style="border-bottom: 2px solid grey;padding:5px;">What is Entity Relationship Diagram in DBMS Entity Relationship Diagram? <span style="float: right;">17th April,2020</span></p>
+			</div>
+			<div class="col-sm-12" style="background-color: #4db8ff;text-align: right;padding:1px;"><h4>See More Questions</h4></div>
+		</div>
+		<div class="col-md-1 col-lg-1 col-sm-12"></div>
+		<div class="col-md-5 col-lg-5 col-sm-12" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);padding: 0px 0px;">
+			<div class="col-sm-12" style="background-color: #66e066;"><h3>DBMS</h3></div>
+			<div style="padding: 10px 10px;">
+				<p>What is DBMS?</p>
+				<p>What is DBMS?</p>
+				<p>What is DBMS?</p>
+			</div>
+			<div class="col-sm-12" style="background-color: #4db8ff;text-align: right;"><h5>See More Questions</h5></div>
+		</div>
+		<div class="col-md-12 col-lg-12 col-sm-12"><p>&nbsp;</p></div>
+		<div class="col-md-5 col-lg-5 col-sm-12" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);padding: 0px 0px;">
+			<div class="col-sm-12" style="background-color: #66e066;"><h3>DBMS</h3></div>
+			<div style="padding: 10px 10px;">
+				<p>What is DBMS?</p>
+				<p>What is DBMS?</p>
+				<p>What is DBMS?</p>
+			</div>
+			<div class="col-sm-12" style="background-color: #4db8ff;text-align: right;"><h5>See More Questions</h5></div>
+		</div>
+		<div class="col-md-1 col-lg-1 col-sm-1"></div>
+		<div class="col-md-5 col-lg-5 col-sm-5" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);padding: 0px 0px;">
+			<div class="col-sm-12" style="background-color: #66e066;"><h3>DBMS</h3></div>
+			<div style="padding: 10px 10px;">
+				<p>What is DBMS?</p>
+				<p>What is DBMS?</p>
+				<p>What is DBMS?</p>
+			</div>
+			<div class="col-sm-12" style="background-color: #4db8ff;text-align: right;"><h5>See More Questions</h5></div>
+		</div>
+		<div class="col-md-5 col-lg-5 col-sm-5" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);padding: 0px 0px;">
+			<div class="col-sm-12" style="background-color: #66e066;"><h3>DBMS</h3></div>
+			<div style="padding: 10px 10px;">
+				<p>What is DBMS?</p>
+				<p>What is DBMS?</p>
+				<p>What is DBMS?</p>
+			</div>
+			<div class="col-sm-12" style="background-color: #4db8ff;text-align: right;"><h5>See More Questions</h5></div>
+		</div>
+		<div class="col-md-1 col-lg-1 col-sm-1"></div>
+		<div class="col-md-5 col-lg-5 col-sm-5" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);padding: 0px 0px;">
+			<div class="col-sm-12" style="background-color: #66e066;"><h3>DBMS</h3></div>
+			<div style="padding: 10px 10px;">
+				<p>What is DBMS?</p>
+				<p>What is DBMS?</p>
+				<p>What is DBMS?</p>
+			</div>
+			<div class="col-sm-12" style="background-color: #4db8ff;text-align: right;"><h5>See More Questions</h5></div>
+		</div>
+	</div>
+</div>
+<?php
+include "Footer.php";
+?>
