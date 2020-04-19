80841ee9f60efac20df727fa2e8667e8ba3fd806
kuntalsarkar20 <62298200+kuntalsarkar20@users.noreply.github.com>
2020-04-19 22:38:34 +0530

    Footer Started

--- Footer.php
+++ Footer.php
@@ -1,2 +1,21 @@
+<footer style="background-color: black;padding:20px 0px;">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-sm-12" style="height:300px;color:white;">
+				<h3>CSQueries</h3>
+				<p>Category</p>
+				<p>Quetions</p>
+				<p>Answers</p>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-12">
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-12">
+			</div>
+		</div>
+	</div>
+</footer>
 </body>
+<script>
+  AOS.init();
+</script>
 </html>
