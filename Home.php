d9f4c0bbb35807fd072007890520fe6c590ba7ea
kuntalsarkar20 <62298200+kuntalsarkar20@users.noreply.github.com>
2020-04-19 16:18:41 +0530

    Updated Home

--- Home.php
+++ Home.php
@@ -1,74 +1,72 @@
 <?php
 include "Header.php";
 ?>
-<div class="container-fluid">
+<div>
 	<img src="background.jpg" width="100%" height="600px">
 	<div class="centered"><center><h2 style="font-weight: bold;">Student Portal for Better UnderStanding</h2><br><h4>Contribute here by Writing Answers for your favourite Subjects and Help others to understand the Subject Easily.</h4><br><button type="button" class="btn btn-success btn-lg">Contribute Here</button></center></div>
 </div>
+<section>
+	<div class="container" style="padding: 20px 0px 50px 0px;">
+		<center><h2 style="font-weight: bold;"><span style="border-bottom:4px solid black;">CSQueries</span></h2></center>
+		<div class="col-lg-3 col-md-3 col-sm-12"></div>
+		<div class="col-lg-6 col-md-6 col-sm-12" style="font-size:15px;">
+			<center>CSQueries is a Open Platform for Students to slove their Computer Science Doubts. CSQueries provides an easy and pointWise solution for the tough Questions. This platform contains a huge number of CS questions for the important Subjects like DataBase Management System, Data Structure & Algorithm, Operating System and many more computer Programming Languages.</center>
+		</div>
+	</div>
+</section>
+<section style="background-color: #e7eeef;">
 <div class="container" style="padding-top: 20px;">
 	<h3 style="font-weight: bold;">Browse By Categories</h3>
 	<hr style="border:2px solid #66e066;">
-	<div class="row" style="padding-left: 30px;padding-right: 30px;">
-		<div class="col-md-5 col-lg-5 col-sm-12" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);padding: 0px 0px;">
-			<div class="col-sm-12" style="background-color: #66e066;padding:1px;"><h3>DBMS</h3></div>
-			<div style="padding: 10px 10px;">
-				<p style="border-bottom: 2px solid grey;padding:5px;">What is DBMS? <span style="float: right;">17th April,2020</span></p>
-				<p style="border-bottom: 2px solid grey;padding:5px;">What is DBMS? <span style="float: right;">17th April,2020</span></p>
-				<p style="border-bottom: 2px solid grey;padding:5px;">What is Entity Relationship Diagram in DBMS Entity Relationship Diagram? <span style="float: right;">17th April,2020</span></p>
+	<div class="row">
+<!-- 		<div class="col-md-1 col-lg-1 col-sm-12"></div> -->
+		<div class="col-md-6 col-lg-6 col-sm-12" style="padding: 30px;">
+			<div class="row" style="box-shadow: 0px 0px 15px 9px rgba(0,0,0,.2);background-color: white;">
+				<div class="col-sm-12" style="background-color: #66e066;padding:1px;"><h3>&nbsp;DBMS</h3></div>
+				<div class="col-sm-12" style="padding: 10px 10px;">
+					<div class="col-sm-12" style="padding:5px;">What is DBMS? <span style="float: right;color:grey;">-17th April,2020</span></div>
+					<div class="col-sm-12" style="padding:5px;">What is DBMS? <span style="float: right;">-17th April,2020</span></div>
+					<div class="col-sm-12" style="padding:5px;">What is Entity Relationship Diagram in DBMS Entity Relationship Diagram? <span style="float: right;">-17th April,2020</span></div>
+				</div>
+				<div class="col-sm-12" style="background-color: #4db8ff;text-align: right;padding:1px;"><h4>See More Questions&nbsp;</h4></div>
 			</div>
-			<div class="col-sm-12" style="background-color: #4db8ff;text-align: right;padding:1px;"><h4>See More Questions</h4></div>
 		</div>
-		<div class="col-md-1 col-lg-1 col-sm-12"></div>
-		<div class="col-md-5 col-lg-5 col-sm-12" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);padding: 0px 0px;">
-			<div class="col-sm-12" style="background-color: #66e066;"><h3>DBMS</h3></div>
-			<div style="padding: 10px 10px;">
-				<p>What is DBMS?</p>
-				<p>What is DBMS?</p>
-				<p>What is DBMS?</p>
+		<div class="col-md-6 col-lg-6 col-sm-12" style="padding: 30px;">
+			<div class="row" style="box-shadow: 0px 0px 15px 9px rgba(0,0,0,.2);background-color: white;">
+				<div class="col-sm-12" style="background-color: #66e066;padding:1px;"><h3>&nbsp;DBMS</h3></div>
+				<div class="col-sm-12" style="padding: 10px 10px;">
+					<div class="col-sm-12" style="padding:5px;">What is DBMS? <span style="float: right;color:grey;">-17th April,2020</span></div>
+					<div class="col-sm-12" style="padding:5px;">What is DBMS? <span style="float: right;">-17th April,2020</span></div>
+					<div class="col-sm-12" style="padding:5px;">What is Entity Relationship Diagram in DBMS Entity Relationship Diagram? <span style="float: right;">-17th April,2020</span></div>
+				</div>
+				<div class="col-sm-12" style="background-color: #4db8ff;text-align: right;padding:1px;"><h4>See More Questions&nbsp;</h4></div>
 			</div>
-			<div class="col-sm-12" style="background-color: #4db8ff;text-align: right;"><h5>See More Questions</h5></div>
 		</div>
-		<div class="col-md-12 col-lg-12 col-sm-12"><p>&nbsp;</p></div>
-		<div class="col-md-5 col-lg-5 col-sm-12" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);padding: 0px 0px;">
-			<div class="col-sm-12" style="background-color: #66e066;"><h3>DBMS</h3></div>
-			<div style="padding: 10px 10px;">
-				<p>What is DBMS?</p>
-				<p>What is DBMS?</p>
-				<p>What is DBMS?</p>
+		<div class="col-md-6 col-lg-6 col-sm-12" style="padding: 30px;">
+			<div class="row" style="box-shadow: 0px 0px 15px 9px rgba(0,0,0,.2);background-color: white;">
+				<div class="col-sm-12" style="background-color: #66e066;padding:1px;"><h3>&nbsp;DBMS</h3></div>
+				<div class="col-sm-12" style="padding: 10px 10px;">
+					<div class="col-sm-12" style="padding:5px;">What is DBMS? <span style="float: right;color:grey;">-17th April,2020</span></div>
+					<div class="col-sm-12" style="padding:5px;">What is DBMS? <span style="float: right;">-17th April,2020</span></div>
+					<div class="col-sm-12" style="padding:5px;">What is Entity Relationship Diagram in DBMS Entity Relationship Diagram? <span style="float: right;">-17th April,2020</span></div>
+				</div>
+				<div class="col-sm-12" style="background-color: #4db8ff;text-align: right;padding:1px;"><h4>See More Questions&nbsp;</h4></div>
 			</div>
-			<div class="col-sm-12" style="background-color: #4db8ff;text-align: right;"><h5>See More Questions</h5></div>
 		</div>
-		<div class="col-md-1 col-lg-1 col-sm-1"></div>
-		<div class="col-md-5 col-lg-5 col-sm-5" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);padding: 0px 0px;">
-			<div class="col-sm-12" style="background-color: #66e066;"><h3>DBMS</h3></div>
-			<div style="padding: 10px 10px;">
-				<p>What is DBMS?</p>
-				<p>What is DBMS?</p>
-				<p>What is DBMS?</p>
+		<div class="col-md-6 col-lg-6 col-sm-12" style="padding: 30px;">
+			<div class="row" style="box-shadow: 0px 0px 15px 9px rgba(0,0,0,.2);background-color: white;">
+				<div class="col-sm-12" style="background-color: #66e066;padding:1px;"><h3>&nbsp;DBMS</h3></div>
+				<div class="col-sm-12" style="padding: 10px 10px;">
+					<div class="col-sm-12" style="padding:5px;">What is DBMS? <span style="float: right;color:grey;">-17th April,2020</span></div>
+					<div class="col-sm-12" style="padding:5px;">What is DBMS? <span style="float: right;">-17th April,2020</span></div>
+					<div class="col-sm-12" style="padding:5px;">What is Entity Relationship Diagram in DBMS Entity Relationship Diagram? <span style="float: right;">-17th April,2020</span></div>
+				</div>
+				<div class="col-sm-12" style="background-color: #4db8ff;text-align: right;padding:1px;"><h4>See More Questions&nbsp;</h4></div>
 			</div>
-			<div class="col-sm-12" style="background-color: #4db8ff;text-align: right;"><h5>See More Questions</h5></div>
-		</div>
-		<div class="col-md-5 col-lg-5 col-sm-5" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);padding: 0px 0px;">
-			<div class="col-sm-12" style="background-color: #66e066;"><h3>DBMS</h3></div>
-			<div style="padding: 10px 10px;">
-				<p>What is DBMS?</p>
-				<p>What is DBMS?</p>
-				<p>What is DBMS?</p>
-			</div>
-			<div class="col-sm-12" style="background-color: #4db8ff;text-align: right;"><h5>See More Questions</h5></div>
-		</div>
-		<div class="col-md-1 col-lg-1 col-sm-1"></div>
-		<div class="col-md-5 col-lg-5 col-sm-5" style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);padding: 0px 0px;">
-			<div class="col-sm-12" style="background-color: #66e066;"><h3>DBMS</h3></div>
-			<div style="padding: 10px 10px;">
-				<p>What is DBMS?</p>
-				<p>What is DBMS?</p>
-				<p>What is DBMS?</p>
-			</div>
-			<div class="col-sm-12" style="background-color: #4db8ff;text-align: right;"><h5>See More Questions</h5></div>
 		</div>
 	</div>
 </div>
+</section>
 <?php
 include "Footer.php";
 ?>
