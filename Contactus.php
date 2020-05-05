74bbe752b08a78506f25eb967af14a10ce388273
afsuyeasu30 <64533156+afsuyeasu30@users.noreply.github.com>
2020-05-05 10:19:15 -0700

    changes

---
+++ Contactus.php
@@ -0,0 +1,35 @@
+<?php
+include "Header.php";
+?>
+<section style="padding: 80px 0px 0px 0px;">
+	<div class="container">
+		<div class="col-lg-2 col-md-2 col-sm-12">&nbsp;</div>
+		<div class="col-lg-8 col-md-8 col-sm-12 box1" style="">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12" style="border: px solid black;padding: 0px 0px 0px 0px;">
+					<img src="10333_c.png" alt="Jane" style="width: 100%;"> 
+				</div>
+				<div class="col-lg-12 col-md-12 col-sm-12 box1" style="padding: 50px 10px;">
+					<form class="form-horizontal">
+					      <div class="col-sm-offset-2 col-sm-8 input-group">
+						      <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+						      <input id="name" type="text" class="form-control" name="name" placeholder="Enter Your Name" required>
+						   </div>
+				    	    <br>
+					    	<div class="col-sm-offset-2 col-sm-8 input-group">
+						      <span class="input-group-addon"><i class="glyphicon glyphicon-folder-open"></i></span>
+						      <input type="name" class="form-control" id="uname" placeholder="Enter Your Email" name="name">
+						   </div>
+					    <br>
+					      <div class="col-sm-offset-2 col-sm-8 input-group">
+						      <span class="input-group-addon"><i class="glyphicon glyphicon-text-size"></i></span>
+						      <textarea class="form-control" rows="5" cols="" style="" id="comment" placeholder="Comments...."></textarea>
+						   </div>
+					    	<br>
+				    	<center><button type="submit" class="contactus-button">Submit</button></center>
+				 	 </form>
+				 </div>
+			</div>
+		</div>
+	</div>
+</section>
