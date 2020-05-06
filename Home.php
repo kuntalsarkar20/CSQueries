91adc222f5d49a39fde925161d06e0d3278a8113
afsuyeasu30 <64533156+afsuyeasu30@users.noreply.github.com>
2020-05-06 21:38:58 +0530

    changes

--- Home.php
+++ Home.php
@@ -1,19 +1,26 @@
 <?php
 include "Header.php";
 ?>
-<div>
-	<img src="background.jpg" width="100%" height="600px">
-	<div class="centered"><center><h2 style="font-weight: bold;">Student Portal for Better UnderStanding</h2><br><h4>Contribute here by Writing Answers for your favourite Subjects and Help others to understand the Subject Easily.</h4><br><button type="button" class="btn btn-success btn-lg">Contribute Here</button></center></div>
+<div class="parallax" style="padding: 500px 0px 50px 0px;">
+	<div class="centered" style="padding: 150px 0px 0px 0px;"><center><h2 style="font-weight: bold;">Student Portal for Better UnderStanding</h2><br><h4>Contribute here by Writing Answers for your favourite Subjects and Help others to understand the Subject Easily.</h4><br><button type="button" class="btn btn-success btn-lg">Contribute Here</button></center></div>
 </div>
 <section>
-	<div class="container" style="padding: 20px 0px 50px 0px;">
+	<div class="container" style="padding: 0px 0px 0px 0px;">
 		<center><h2 style="font-weight: bold;"><span style="border-bottom:4px solid black;">CSQueries</span></h2></center>
 		<div class="col-lg-2 col-md-2 col-sm-12"></div>
 		<div class="col-lg-8 col-md-8 col-sm-12" style="font-size:15px;">
 			<center>CSQueries is a Open Platform for Students to slove their Computer Science Doubts. CSQueries provides an easy and pointWise solution for the tough Questions. This platform contains a huge number of CS questions for the important Subjects like DataBase Management System, Data Structure & Algorithm, Operating System and many more computer Programming Languages.</center><br><br>
 		</div>
 		<div class="row">
-			<div class="col-lg-3 col-md-3 col-sm-12">
+			<div class="col-lg-3 col-md-3 col-sm-12" style="padding: 30px;">
+				<div class="card">
+				  <img src="write_for_us_2.png" alt="John" style="width:100%;height:200px;">
+				  <h3>Write For Us</h3>
+				  <p class="title"><center><button type="button" class="btn btn-primary btn-md">Start Here</button></center></p>
+				   <br>
+				</div>
+			</div>
+			<!-- <div class="col-lg-3 col-md-3 col-sm-12" style="">
 				<div class="card">
 				  <img src="write_for_us_2.png" alt="John" style="width:100%;height:200px;">
 				  <h3>Write For Us</h3>
@@ -24,21 +31,21 @@ include "Header.php";
 			<div class="col-sm-1"></div>
 			<div class="col-lg-4 col-md-4 col-sm-12">
 				<div class="card">
-				  <img src="having_doubts.png" alt="John" style="width:100%">
+				  <img src="having_doubts.png" alt="John" style="width:100%;">
 				  <h3>Having Doubts?</h3>
 				  <p class="title"><center><button type="button" class="btn btn-primary btn-md">Ask Us</button></center></p>
 				   <br>
 				</div>
-			</div>
-			<div class="col-sm-1"></div>
-			<div class="col-lg-3 col-md-3 col-sm-12">
+			</div> -->
+			<!-- <div class="col-sm-1"></div> -->
+			<!-- <div class="col-lg-3 col-md-3 col-sm-12">
 				<div class="card">
 				  <img src="search_here.png" alt="John" style="width:100%">
 				  <h3>Find Answers Quickly</h3>
 				  <p class="title"><center><button type="button" class="btn btn-primary btn-md">Search</button></center></p>
 				  <br>
 				</div>
-			</div>
+			</div> -->
 		</div>
 	</div>
 </section>
