ddb24013ed6cf31128ac0e9cdc6e52d461e177eb
Afsara Yeasmin <afsarayeasmin30@gmail.com>
2020-05-31 22:38:59 +0530

    Add Some

--- ViewAllQus.php
+++ ViewAllQus.php
@@ -4,8 +4,9 @@ include "Header.php";
 <section style="padding: 80px 0px 0px 0px;">
 <div class="container">
 	<div class="col-md-offset-1 col-lg-10 col-md-10 col-md-10 box1" style="padding: 20px 0px 0px 0px;border:px solid black;">
-		<div class="row">
-			<div class="col-md-offset-2 col-lg-8 col-md-8 col-sm-8">
+		<div class="row" style="padding:15px;">
+			<div class="col-lg-2 col-md-2 col-sm-12">&nbsp;</div>
+			<div class="col-lg-8 col-md-8 col-sm-8" style="padding: px;">
 				<div class="input-group">
 		        <input type="text" class="form-control" placeholder="Search" name="search">
 		        <div class="input-group-btn">
@@ -15,7 +16,8 @@ include "Header.php";
 		        </div>
 		      </div>
 			</div>
-			<div class="col-md-offset-1 col-lg-10 col-md-10 col-sm-10">
+			<div class="col-lg-2 col-md-2 col-sm-12">&nbsp;</div>
+			<div class="col-lg-offset-1 col-md-offset-1 col-lg-10 col-md-10 col-sm-12">
 				<h2>Qus: Why is the use of DBMS recommended? Explain by listing some of its major advantages.</h2>
 				<h2>Qus: Why is the use of DBMS recommended? Explain by listing some of its major advantages.</h2>
 				<h2>Qus: Why is the use of DBMS recommended? Explain by listing some of its major advantages.</h2>
